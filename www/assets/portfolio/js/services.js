277f33b2e06dc73189fdcf71769168af5120a910
Adam Presley <adam@adampresley.com>
2015-01-27 19:28:24 -0600

    Removed functions that carried over from another project

--- www/assets/portfolio/js/services.js
+++ www/assets/portfolio/js/services.js
@@ -12,20 +12,6 @@ services.Util = {
 				callback();
 			}
 		});
-	},
-
-	setMenuItemAsCurrent: function(menuItem) {
-		var menuItems = document.getElementsByClassName("menuItem");
-
-		for (var index = 0; index < menuItems.length; index++) {
-			document.getElementById(menuItems[index].id).classList.remove("pure-menu-selected");
-		}
-
-		document.getElementById(menuItem + "Menu").classList.add("pure-menu-selected");
-	},
-
-	setPageTitle: function(title) {
-		document.getElementById("pageTitle").innerHTML = title;
 	}
 };
 
